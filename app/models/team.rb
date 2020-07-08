ba94e4d9f36e245b2f96416b6dfda966a661f714
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-08 18:49:23 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,9 +3,14 @@ attr_accessor :name, :motto
 
 @@all = []
 
-def initialize(name, motto)
-  @name = name
-  @motto = motto
+  def initialize(name, motto)
+    @name = name
+    @motto = motto
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
 
 end
